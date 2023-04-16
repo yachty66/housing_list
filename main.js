902b42515e46ff180efecf311804604e5790d016
Max Hager <maxhager28@gmail.com>
2023-04-16 13:27:57 -0700

    empty form

--- main.js
+++ main.js
@@ -1,38 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("add-community-form");
     form.addEventListener("submit", (event) => {
-      event.preventDefault();
+        event.preventDefault();
 
-      const name = document.getElementById("name").value.trim();
-      const link = document.getElementById("link").value.trim();
-      const status = document.getElementById("status").value.trim();
-      const city = document.getElementById("city").value.trim();
+        const name = document.getElementById("name").value.trim();
+        const link = document.getElementById("link").value.trim();
+        const status = document.getElementById("status").value.trim();
+        const city = document.getElementById("city").value.trim();
 
-      // Inside the form event listener, replace the console.log with this code:
-      if (name === "" || link === "" || status === "" || city === "") {
-        alert("Please fill out all fields before submitting.");
-      } else {
-        //todo change to real domain
-        fetch("https://thehousinglist.uw.r.appspot.com/submit_community", { 
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ name, link, status, city }),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.success) {
-              alert("New intentional community has been added and gets validated by maintainer.");
-            } else {
-              alert(
-                "There was an error submitting the form. Please try again."
-              );
-            }
-          })
-          .catch((error) => {
-            console.error("Error submitting form:", error);
-          });
-      }
+        if (name === "" || link === "" || status === "" || city === "") {
+            alert("Please fill out all fields before submitting.");
+        } else {
+            fetch("https://thehousinglist.uw.r.appspot.com/submit_community", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ name, link, status, city }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    if (data.success) {
+                        alert("New intentional community has been added and gets validated by maintainer.");
+                        // Reset the form after a successful submission
+                        form.reset();
+                    } else {
+                        alert("There was an error submitting the form. Please try again.");
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error submitting form:", error);
+                });
+        }
     });
-  });
+});
